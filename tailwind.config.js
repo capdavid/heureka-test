ab9b0485a949670884ef2962f5601f44bcc99e38
capdavid <david.cap@applifting.cz>
2021-07-19 13:01:38 +0200

    Remove next boilerplate, setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./**/*.{ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      10: ["10px", "10px"],
+      12: ["12px", "12px"],
+      14: ["14px", "14px"],
+      16: ["16px", "26px"],
+      18: ["18px", "30px"],
+      22: ["22px", "26px"],
+      24: ["24px", "29px"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
